0406f7b6d5e55b2459b93a5d14f43ee881ef79d1
furkanekiz <furkanekiz.06@gmail.com>
2022-08-15 10:07:06 +0300

    Transforming Data Between Destinations

--- app/src/main/java/com/furkanekiz/navigationdemo/FRHome.kt
+++ app/src/main/java/com/furkanekiz/navigationdemo/FRHome.kt
@@ -1,10 +1,13 @@
 package com.furkanekiz.navigationdemo
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.furkanekiz.navigationdemo.databinding.FrHomeBinding
@@ -18,12 +21,16 @@ class FRHome : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        // Inflate the layout for this fragment
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fr_home, container, false)
 
         binding.btnFRHome.setOnClickListener {
-            it.findNavController().navigate(R.id.action_FRHome_to_FRSecond)
+            if (!TextUtils.isEmpty(binding.etFRHome.text.toString())){
+                val bundle: Bundle = bundleOf("user_input" to binding.etFRHome.text.toString())
+                it.findNavController().navigate(R.id.action_FRHome_to_FRSecond,bundle)
+            }else{
+                Toast.makeText(activity,"Please insert your name",Toast.LENGTH_LONG).show()
+            }
         }
 
         return binding.root
--- app/src/main/java/com/furkanekiz/navigationdemo/FRSecond.kt
+++ app/src/main/java/com/furkanekiz/navigationdemo/FRSecond.kt
@@ -16,9 +16,11 @@ class FRSecond : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fr_second, container, false)
+
+        val input = requireArguments().getString("user_input")
+        binding.tvFRSecond.text = input.toString()
 
         return binding.root
     }
